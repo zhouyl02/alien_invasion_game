d3c1c2466b2a88794b6be41beddd7b6e556daf3b
Zhouyl <915186072@qq.com>
2018-02-01 20:34:49 +0800

    settings

---
+++ alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings():
+	"""存储《外星人入侵》的所有设置的类"""
+	
+	def _init_(self):
+		"""初始化游戏的设置"""
+		#屏幕设置
+		self.screen_width = 1100
+		self.screen_height = 650
+		self.bg_color = (230, 230, 230)
+		
